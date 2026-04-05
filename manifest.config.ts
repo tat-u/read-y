c8ceac0ad774dee95cb847697fce1f8e5a24ab90
tat-u <uetatsu1005@gmail.com>
2026-04-05 16:52:36 +0900

    chore: Set display name of extension to `"Read-y"`

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,28 +1,27 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: "Read-y", // NOTE: package.json とは別途で拡張機能の名前を指定している
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    48: "public/logo.png",
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      48: "public/logo.png",
     },
-    default_popup: 'src/popup/index.html',
+    default_popup: "src/popup/index.html",
   },
-  content_scripts: [{
-    js: ['src/content/main.ts'],
-    matches: ['https://*/*'],
-  }],
-  permissions: [
-    'sidePanel',
-    'contentSettings',
+  content_scripts: [
+    {
+      js: ["src/content/main.ts"],
+      matches: ["https://*/*"],
+    },
   ],
+  permissions: ["sidePanel", "contentSettings"],
   side_panel: {
-    default_path: 'src/sidepanel/index.html',
+    default_path: "src/sidepanel/index.html",
   },
-})
+});
